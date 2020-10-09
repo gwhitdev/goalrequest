102f3ce087f50317ea37e4192ca818ac45316143
Gareth Whitley <me@garethwhitley.online>
2020-10-09 13:18:11 +0100

    made sure program ensures total amount of pages are requested and info stored to totalNumberOfGoals

--- r3.js
+++ r3.js
@@ -2,49 +2,60 @@ const axios = require('axios');
 const fs = require('fs');
 
 /* Create a new async request that gets all the matches that have team1 or team2 named as the param 'team'. 
-Then, add up all the team1 and team2 goals as aproppriate and add them, respectively, to individual variables.*/
+Then, add up all the team1 and team2 goals as appropriate and add them, respectively, to individual variables.*/
 
 const team = 'Barcelona';
 const year = '2011';
-const FIRST_URL = `https://jsonmock.hackerrank.com/api/football_matches?competition=UEFA%20Champions%20League&year=${year}&team1=${team}`;
-const SECOND_URL = `https://jsonmock.hackerrank.com/api/football_matches?competition=UEFA%20Champions%20League&year=${year}&team2=${team}`;
+const MAIN_URL = `https://jsonmock.hackerrank.com/api/football_matches?competition=UEFA%20Champions%20League&year=${year}&team1=${team}`
+const SECOND_MAIN_URL = `https://jsonmock.hackerrank.com/api/football_matches?competition=UEFA%20Champions%20League&year=${year}&team2=${team}`
 
 const getPages = async (team, year) => {
-    console.log('Making first connection...')
-    const firstResponse = await axios.get(FIRST_URL);
-    console.log('First response received.')
+    const mainResponse = await axios.get(MAIN_URL);
+    const secondMainResponse = await axios.get(SECOND_MAIN_URL);
 
-    console.log('Making second connection...')
-    const secondResponse = await axios.get(SECOND_URL);
-    console.log('Second response received.')
+    let firstNumberOfPages = mainResponse.data.total_pages;
+    let secondNumberOfPages = secondMainResponse.data.total_pages;
 
-    let numberOfPages = firstResponse.data.total_pages;
-    let secondNumOfPages = secondResponse.data.total_pages;
-    let numberOfMatches = firstResponse.data.total;
-    let secondNumberOfMatches = secondResponse.data.total;
+    
+    let teamOneMatches = mainResponse.data.total;
+    let teamTwoMatches = secondMainResponse.data.total;
 
-    //console.log('Num of pages 1: ', numberOfPages);
-    //console.log('Num of pages 2: ', secondNumOfPages);
+    console.log('Num of pages 1: ', firstNumberOfPages);
+    console.log('Num of pages 2: ', secondNumberOfPages);
 
     let numberOfGoals = 0;
+
     console.log('Getting team 1 goals...')
-        for (let i = 0; i < numberOfMatches; i++) {
-           numberOfGoals += Number(firstResponse.data.data[i].team1goals);
-            //console.log('Iterate team1 number of goals: ', numberOfGoals);
+
+    let firstResponse = {};
+    for (let i = 0 ; i < firstNumberOfPages; i++) {
+        let response = await axios.get(`https://jsonmock.hackerrank.com/api/football_matches?competition=UEFA%20Champions%20League&year=${year}&team1=${team}&page=${i}`)
+        for (let i = 0; i < teamOneMatches; i++) {
+            numberOfGoals += Number(response.data.data[i].team1goals);
+            console.log('Iterate team1 number of goals: ', numberOfGoals);
         }
+        firstResponse = response.data.data;
+    }
+    let secondResponse = {};
     console.log('Getting team 2 goals...')
-        for (let i = 0; i < secondNumberOfMatches; i++) {
-            numberOfGoals += Number(secondResponse.data.data[i].team2goals);
-            //console.log('Iterate team2 number of goals: ', numberOfGoals);
+    for (let i = 0 ; i < secondNumberOfPages; i++) {
+        let response = await axios.get(`https://jsonmock.hackerrank.com/api/football_matches?competition=UEFA%20Champions%20League&year=${year}&team2=${team}&page=${i}`)
+        
+        for (let i = 0; i < teamTwoMatches; i++) {
+            numberOfGoals += Number(response.data.data[i].team2goals);
+            console.log('Iterate team2 number of goals: ', numberOfGoals);
         }
-    let resOne = JSON.stringify(firstResponse.data.data);
-    let resTwo = JSON.stringify(secondResponse.data.data);
+        secondResponse = response.data.data;
+    }
+        
+    let r1 = JSON.stringify(firstResponse);
+    let r2 = JSON.stringify(secondResponse);
 
     console.log('Creating result object...')
 
     let response = {
-        r1: resOne,
-        r2: resTwo,
+        r1: r1,
+        r2: r2,
         r3: numberOfGoals
     }
     
@@ -53,7 +64,9 @@ const getPages = async (team, year) => {
     return response;
 }
 
+
 const writeToFile = async (result) => {
+    console.log(result);
     console.log('Writing to file one...')
     await fs.writeFile('result1.json', result.r1, (err) => err);
     console.log('Writing to file two...')
